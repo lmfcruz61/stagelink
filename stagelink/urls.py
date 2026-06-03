1f0a64603b83a0c4883540261c95e3bdb2603a48
lmfcruz61 <16322178+lmfcruz61@users.noreply.github.com>
2026-06-03 16:07:55 +0100

    Serve media route explicitly in production

--- stagelink/urls.py
+++ stagelink/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf import settings
-from django.conf.urls.static import static
 from django.urls import include, path
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,4 +30,6 @@ urlpatterns = [
 
 # No piloto em Fly.io, os uploads do admin ficam no volume montado em MEDIA_ROOT.
 # Em escala maior, isto deve passar para S3/R2 ou outro object storage.
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    path('media/<path:path>', serve, {'document_root': settings.MEDIA_ROOT}),
+]
