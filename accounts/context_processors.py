3bc6b33bf5f2c13250b5364f223293e933945508
lmfcruz61 <16322178+lmfcruz61@users.noreply.github.com>
2026-06-04 18:16:38 +0100

    Version site appearance media URLs

--- accounts/context_processors.py
+++ accounts/context_processors.py
@@ -2,6 +2,9 @@ from .models import SiteAppearance
 
 
 def site_appearance(request):
+    appearance = SiteAppearance.objects.first()
+    version = int(appearance.updated_at.timestamp()) if appearance and appearance.updated_at else ''
     return {
-        'site_appearance': SiteAppearance.objects.first(),
+        'site_appearance': appearance,
+        'site_appearance_version': version,
     }
